02fe6ca3f9ee87644b792c8b5080545167a8346c
star362 <star362@163.com>
2023-12-15 11:50:25 +0800

    code

--- springbean-demo/src/main/java/com/star/springbeandemo/controller/TestController.java
+++ springbean-demo/src/main/java/com/star/springbeandemo/controller/TestController.java
@@ -1,25 +1,30 @@
 package com.star.springbeandemo.controller;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import com.star.springbeandemo.config.SpringBeanUtils;
-import com.star.springbeandemo.config.TerminateBean;
 import com.star.springbeandemo.service.AService;
 import com.star.springbeandemo.zhoenjiemodel.Market;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.Future;
 
 /**
  * @author wangyu
@@ -28,50 +33,53 @@ import java.util.concurrent.Future;
  */
 @RestController
 @Slf4j
-public class TestController  {
-
+public class TestController {
 
 
     @Autowired
     ConfigurableApplicationContext context;
 
 
-
     @Autowired
     ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
 
-
-
     @GetMapping("/test")
-    public String test(){
+    public String test() {
 
         return "close success";
     }
 
 
-
-
     @GetMapping("/aa")
-    public String aa(){
+    public String aa() {
 
         threadPoolTaskExecutor.execute(() -> {
 
-            int i=0;
-            while (i<200000) {
-            while (i<200000) {while (i<200000) {while (i<200000) {while (i<200000) {while (i<200000) {}}}}}
+            int i = 0;
+            while (i < 200000) {
+                while (i < 200000) {
+                    while (i < 200000) {
+                        while (i < 200000) {
+                            while (i < 200000) {
+                                while (i < 200000) {
+                                }
+                            }
+                        }
+                    }
+                }
 
                 /*try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }*/
-                Random random=new Random();
+                Random random = new Random();
                 random.nextInt(10);
-                System.out.println("===="+i+":"+Thread.currentThread().getName());
+                System.out.println("====" + i + ":" + Thread.currentThread().getName());
                 i++;
             }
-            System.out.println("runnable===========:"+Thread.currentThread().getName());
+            System.out.println("runnable===========:" + Thread.currentThread().getName());
         });
 
 
@@ -80,8 +88,8 @@ public class TestController  {
 
 
     @GetMapping("/aaa")
-    public String aaa(){
-        
+    public String aaa() {
+
         log.info("==========aaaaa=========");
 
         return "success";
@@ -91,7 +99,7 @@ public class TestController  {
     private Market market;
 
     @GetMapping("/aa1")
-    public String aa1(){
+    public String aa1() {
 
 
         market.selfAction();
@@ -103,26 +111,47 @@ public class TestController  {
 
 
     @GetMapping("/getbean1")
-    public void getbean1(){
+    public void getbean1() {
         final ApplicationContext context = SpringBeanUtils.getContext();
-        ConfigurableApplicationContext context1= (ConfigurableApplicationContext) context;
+        ConfigurableApplicationContext context1 = (ConfigurableApplicationContext) context;
         final DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context1.getBeanFactory();
         beanFactory.removeBeanDefinition("AService");
     }
 
     @GetMapping("/getbean")
-    public String[] getbean(){
+    public String[] getbean() {
         SpringBeanUtils.getContext().getBeanDefinitionCount();
         return SpringBeanUtils.getContext().getBeanDefinitionNames();
     }
 
     @Autowired
     private AService aService;
+
     @GetMapping("/aService")
-    public void aService(){
+    public void aService() {
 
         aService.apply();
     }
 
+
+    @GetMapping("/code")
+    public void code(@RequestParam String codeUrl, HttpServletResponse response) throws IOException, WriterException {
+
+        //生成二维码
+        //map中存放的是生成二维码的参数
+        Map<EncodeHintType, Object> map2 = new HashMap<EncodeHintType, Object>();
+        map2.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        //生成二维码，参数依次是：二维码内容，二维码编码方式，长，宽，参数
+        BitMatrix encode = new MultiFormatWriter().encode(codeUrl, BarcodeFormat.QR_CODE, 200, 200, map2);
+
+        //获取输出流，响应到前台
+        OutputStream out = response.getOutputStream();
+
+        MatrixToImageWriter.writeToStream(encode, "jpg", out);
+
+        out.flush();
+        out.close();
+    }
 
 }
